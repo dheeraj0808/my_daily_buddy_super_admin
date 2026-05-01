acefe135760071183ae7d30c4f7c3ca65d8548f7
Dheeraj Singh <dheerajsingh1939@gmail.com>
2026-05-01 23:35:48 +0530

    styling the page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SuperAdminLogin from './components/SuperAdminLogin'
+
+export default function App() {
+  return (
+    <div className="app-root">
+      <h1>Super Admin Login</h1>
+      <SuperAdminLogin />
+    </div>
+  )
+}
