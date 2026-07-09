1631140a7270fcd9116ecbe2028a9374d709ea25
Dheeraj Singh <dheerajsingh1939@gmail.com>
2026-07-09 21:04:17 +0530

    inhancing ui with talwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       colors: {
         brand: {
           50: '#eef2ff',
